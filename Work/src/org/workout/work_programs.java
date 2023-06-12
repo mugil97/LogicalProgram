c69db395b54d7e91cac8494d63a567ccae87bf6f
mugil97 <smugilan02062001@gmail.com>
2023-06-12 17:11:28 +0530

    mugil

---
+++ Work/src/org/workout/work_programs.java
@@ -0,0 +1,14 @@
+package org.workout;
+import java.util.Scanner;
+
+public class work_programs {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		String n = scan.next();
+		System.out.println(n);
+		
+	}
+	
+}
